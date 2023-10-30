4b890177902decf42f1c97c312ea720a6891f58c
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:44:34 +0100

    añadir nueva linea sobre mi nombre

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("What is your name?");
+        System.out.println("My name is AI 3000");
     }
 }
